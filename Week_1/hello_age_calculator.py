119e3715c0e13009451c0845cc115cd362e11aff
blesoh <rapandoblessing@gmail.com>
2025-09-14 02:55:49 +0300

    Day 1 task complete

---
+++ Week_1/hello_age_calculator.py
@@ -0,0 +1,6 @@
+# age calculator asking user their name and age
+name = str(input("What is your name ? "))
+age = int(input("How old are you ? "))
+age = age + 5 # add 5 to age
+# print their name and age in 5 years
+print(f"Hello {name}, you will be {age} years old in 5 years.")
